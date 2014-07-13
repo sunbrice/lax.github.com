a04ea04401c45c8bdd9ec084ff070cd801f3bc90
Liu Lantao <liulantao@gmail.com>
2014-07-13 12:05:26 +0800

    new file:   _plugins/remove-empty-lines-html.rb

---
+++ _plugins/remove-empty-lines-html.rb
@@ -0,0 +1,25 @@
+module Jekyll
+  module Convertible
+    def write(dest)
+      path = destination(dest)
+      FileUtils.mkdir_p(File.dirname(path))
+      if File.extname(path).downcase == '.html' then
+        self.output.strip!
+        reg = /<\/?pre[^>]*>/i
+        pres = self.output.scan(reg)
+        tary = self.output.split(reg)
+        oary = []
+        reg = /^[\s\t]*(\r\n|\r|\n)/
+        tary.each_with_index { |e, i|
+          oary << ( i % 2 != 0 ? e : e.gsub(reg, "\n") )
+          oary << pres[i] if pres.size > i
+          oary << "\n" if pres.size > i and i % 2 != 0
+        }
+        self.output = oary.join("")
+      end
+      File.open(path, 'w') do |f|
+        f.write( self.output )
+      end
+    end
+  end
+end
